7a23a707866911e27c5d26b4bb513e746c90bcf3
abdulfatahi-ishaq <abfatahi.iaf@gmail.com>
2021-04-04 23:59:06 +0100

    Update Last Commit

---
+++ frontend/src/core/pages/user/signin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import Menu from '../../components/menu';
+
+
+const Signin = () => {
+    return (
+    <div className="text-center">SIGNIN PAGE</div>);
+}
+
+export default Signin;
