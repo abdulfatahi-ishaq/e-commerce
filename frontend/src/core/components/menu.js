bd00f7b1591ee3b3532eac4cf64c61ef526b2793
abdulfatahi-ishaq <abfatahi.iaf@gmail.com>
2021-04-05 00:15:01 +0100

    New Commit 05/04/21

--- frontend/src/core/components/menu.js
+++ frontend/src/core/components/menu.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
-const Menu = () => (
+const isActive=(history, path)=>{
+    if(history.location.pathname === path){
+        return {color:'#ff9900'};
+    }else{
+        return {color:'#ffffff'};
+    }
+}
+const Menu = ({history}) => (
     <div>
         <ul className="nav nav-bar nav-tabs bg-dark">
-            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/signin">Signin</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/signup">Signup</Link></li>
+            <li className="nav-item"><Link className="nav-link" style={isActive(history,'/')} to="/">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link" style={isActive(history,'/signin')} to="/signin">Signin</Link></li>
+            <li className="nav-item"><Link className="nav-link" style={isActive(history,'/signup')} to="/signup">Signup</Link></li>
         </ul>
     </div>
 );
