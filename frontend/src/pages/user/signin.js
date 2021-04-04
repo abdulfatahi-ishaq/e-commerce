9f2e5f6bb3a7127952718fa88a1fefd156ef203a
abdulfatahi-ishaq <abfatahi.iaf@gmail.com>
2021-04-04 23:24:52 +0100

    Update 2nd 2 Last Commit

---
+++ frontend/src/pages/user/signin.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Signin = () => {
+    return (<div className="text-center">SIGNIN PAGE</div>);
+}
+
+export default Signin;
---
+++ frontend/src/pages/user/signup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Signup= () => {
+    return (<div className="text-center">SIGNUP PAGE</div>);
+}
+
+export default Signup;
