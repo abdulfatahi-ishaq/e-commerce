c6bb0dfec632e08fa16c5b3ac67961b804c52599
abdulfatahi-ishaq <abfatahi.iaf@gmail.com>
2021-04-05 00:04:28 +0100

    Initial Commit 05/04/21

--- frontend/src/core/components/menu.js
+++ frontend/src/core/components/menu.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
-const Menu = () => {
+const Menu = () => (
     <div>
-        <ul className="nav nav-bar nav-tabs bg-primary">
+        <ul className="nav nav-bar nav-tabs bg-dark">
             <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/signin">Signin</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/signup">Signup</Link></li>
         </ul>
     </div>
-};
+);
 
 export default withRouter(Menu);
