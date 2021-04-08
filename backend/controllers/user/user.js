0988c28a7388c3af1d11d4fbcd79d1fc181ab62f
abdulfatahi-ishaq <abfatahi.iaf@gmail.com>
2021-04-09 00:39:05 +0100

    Fixed bug Signup ***

--- backend/controllers/user/user.js
+++ backend/controllers/user/user.js
@@ -1,12 +1,24 @@
 let { User } = require("../../core/database/models");
+let { bcrypt } = require("../../modules/imports");
 
 exports.signup = (req, res) => {
-  let { name, email, password } = req.body;
-  const user = new User({ name, email, password });
-  user.save((err, user) => {
-    if (err) {
-      return res.status(400).json({ err });
-    }
-    res.json({ user });
+  let { role, history, name, email, password, createdAt, updatedAt } = req.body;
+  bcrypt.hash(password, 10, (err, hashedpassword) => {
+    if (err) return res.status(400).json({ err });
+    const user = new User({
+      role,
+      history,
+      name,
+      email,
+      password : hashedpassword,
+      createdAt,
+      updatedAt,
+    });
+    user.save((err, user) => {
+      if (err) {
+        return res.status(400).json({ err });
+      }
+      res.json({ user });
+    });
   });
 };
