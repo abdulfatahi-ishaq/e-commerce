dcca5c61136bed5527fc95232b30a6c487a42dec
abdulfatahi-ishaq <abfatahi.iaf@gmail.com>
2021-04-25 11:28:13 +0100

    Added Product Search Logic ***

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,7 +4,7 @@ let router = express.Router();
 const { isAuthorized } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const { isAuth, isAdmin } = require("../core/middlewares/validator");
-const {create,productById,read,remove,update, list, listRelated,listCategories} = require('../controllers/product');
+const {create,productById,read,remove,update, list, listRelated,listCategories,listBySearch} = require('../controllers/product');
 
 //CRUD
 router.post('/product/create/:userId', isAuthorized, isAuth, isAdmin, create);
@@ -16,7 +16,7 @@ router.put('/product/:productId/:userId',isAuthorized, isAuth, isAdmin, update);
 router.get('/products',list);
 router.get('/products/related/:productId',listRelated);
 router.get('/products/categories',listCategories);
-// router.post('/products/search',listBySearch)
+router.post('/products/search',listBySearch);
 
 // Query by Params
 router.param('userId',userById);
